f3d2858d9d4a7acc3a0be961c1747676f7a2800e
numi8462 <yhk8462@naver.com>
2025-12-12 18:22:52 +0900

    feat: 공통 토스트 추가

--- src/pages/DataManagement.tsx
+++ src/pages/DataManagement.tsx
@@ -1,3 +1,17 @@
+import { useToastStore } from "@/store/toastStore";
+
 export default function DataManagement() {
-  return <div className="space-y-3">DataManagement</div>;
+  const { successToast, errorToast } = useToastStore();
+  return (
+    <div className="space-y-3">
+      <div className="flex flex-col">
+        <button onClick={() => successToast("성공적으로 수정되었습니다.")}>
+          성공
+        </button>
+        <button onClick={() => errorToast("수정에 실패하였습니다.")}>
+          실패
+        </button>
+      </div>
+    </div>
+  );
 }
