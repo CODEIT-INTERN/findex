9fcfeeafbb90db43f22a824342e57e8d1c27ae3b
songdaramji <songdaramji@gmail.com>
2025-12-17 14:35:11 +0900

    feat: 지수관리페이지 테이블 컴포넌트 추가

--- src/pages/IndexManagement.tsx
+++ src/pages/IndexManagement.tsx
@@ -1,10 +1,12 @@
 import IndexFilterSection from "@/components/pages/indexManagement/IndexFilterSection";
+import IndexTable from "@/components/pages/indexManagement/IndexTable";
 
 const IndexManagement = () => {
   return (
     <div>
       지수관리
       <IndexFilterSection />
+      <IndexTable />
     </div>
   );
 };
