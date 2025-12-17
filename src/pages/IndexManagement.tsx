9f1d668107a28e90f6b008bc29dc22e518b272d9
Song Jihyun <141377745+songdaramji@users.noreply.github.com>
2025-12-17 13:33:50 +0900

    [Feat] dropdown 스타일 수정 및 지수관리 즐겨찾기 옵션 추가 (#6)
    * chore: 파일명 대문자 수정
    
    * chore: 파일명 대문자 수정
    
    * style: onOpenChange 추가
    
    * feat: 지수 목록 즐겨찾기 필터링 옵션 상수 분리
    
    * feat: 지수 필터 드롭다운 추가
    
    * chore: 지수관리 페이지 경로 수정
    
    * feat: 지수관리 목록 즐겨찾기 필터링 옵션 추가
    
    * feat: 임시 페이지 이동 기능 추가

---
+++ src/pages/IndexManagement.tsx
@@ -0,0 +1,12 @@
+import IndexFilterSection from "@/components/pages/indexManagement/IndexFilterSection";
+
+const IndexManagement = () => {
+  return (
+    <div>
+      지수관리
+      <IndexFilterSection />
+    </div>
+  );
+};
+
+export default IndexManagement;
--- src/pages/Indices.tsx
+++
@@ -1,3 +0,0 @@
-export default function Indices() {
-  return <div className="space-y-3">Indices</div>;
-}
