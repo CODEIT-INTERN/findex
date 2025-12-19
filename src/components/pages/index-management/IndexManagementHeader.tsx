1fdabec6b0da806b2158375097e7d61a3193296f
songdaramji <songdaramji@gmail.com>
2025-12-19 16:50:08 +0900

    feat: 지수 연동 기능 구현

--- src/components/pages/index-management/IndexManagementHeader.tsx
+++ src/components/pages/index-management/IndexManagementHeader.tsx
@@ -1,19 +1,41 @@
 import { useState } from "react";
 import { Plus, RefreshCcw05 } from "@untitledui/icons";
+import { syncIndexInfo } from "@/api/indexInfoApi";
 import { Button } from "@/components/common/buttons/Button";
+import { Dot } from "@/components/common/foundations/dot-icon";
 import ConfirmModal from "@/components/common/modals/ConfirmModal";
+import { useIndexIndexListStore } from "@/store/indexInfoListStore";
+import { useToastStore } from "@/store/toastStore";
 import CreateIndexModal from "./CreateIndexModal";
 
 export const IndexHeader = () => {
-  // 모달 상태들
+  const { fetch } = useIndexIndexListStore();
+  const { successToast, errorToast } = useToastStore();
+  // 모달 및 상태관리
   const [isCreateModalOpen, setCreateModalOpen] = useState(false);
-  const [isLinkModalOpen, setLinkModalOpen] = useState(false);
+  const [isConfirmModalOpen, setConfirmModalOpen] = useState(false);
+  const [isResultModalOpen, setResultModalOpen] = useState(false);
 
+  // 결과 데이터 상태
+  const [syncResult, setSyncResult] = useState({
+    successCount: 0,
+    failCount: 0,
+  });
   const handleCreate = () => {
     setCreateModalOpen(false);
   };
-  const handleLink = () => {
-    setLinkModalOpen(false);
+  const handleSync = async () => {
+    try {
+      const { successCount, failCount } = await syncIndexInfo();
+      setSyncResult({ successCount, failCount });
+      setConfirmModalOpen(false);
+      setResultModalOpen(true);
+      fetch();
+      successToast("연동이 완료되었습니다.");
+    } catch (err) {
+      console.error(err);
+      errorToast("지수 연동에 실패하였습니다.");
+    }
   };
   return (
     <div className="border-secondary flex justify-between border-b px-6 py-5">
@@ -33,7 +55,7 @@ export const IndexHeader = () => {
         <Button
           iconLeading={<RefreshCcw05 size={20} stroke="white" />}
           showTextWhileLoading
-          onClick={() => setLinkModalOpen(true)}
+          onClick={() => setConfirmModalOpen(true)}
         >
           Open API 연동
         </Button>
@@ -46,18 +68,38 @@ export const IndexHeader = () => {
           onOpenChange={setCreateModalOpen}
         />
       )}
-
-      {/* 지수 연동 모달 */}
+      {/* 지수 연동 컨펌 모달 */}
       <ConfirmModal
-        isOpen={isLinkModalOpen}
-        onClose={() => setLinkModalOpen(false)}
-        onConfirm={handleLink}
+        isOpen={isConfirmModalOpen}
+        onClose={() => setConfirmModalOpen(false)}
+        onConfirm={handleSync}
         title="지수 정보 연동"
         variant="primary"
       >
         <p className="text-sm text-gray-500">
           Open API를 통해 최신 지수 정보를 연동합니다.
         </p>
+      </ConfirmModal>
+
+      {/* 지수 연동 결과 모달 */}
+      <ConfirmModal
+        isOpen={isResultModalOpen}
+        onClose={() => setResultModalOpen(false)}
+        onConfirm={() => setResultModalOpen(false)}
+        title="지수 정보 연동"
+        variant="primary"
+        isSingleButton
+      >
+        <div className="text-text-tertiary flex flex-col text-sm">
+          <div className="flex items-center justify-start gap-1">
+            <Dot className="text-fg-success-secondary h-1.5 w-1.5" />
+            <span>성공 {syncResult.successCount}건</span>
+          </div>
+          <div className="flex items-center justify-start gap-1">
+            <Dot className="text-fg-error-primary h-1.5 w-1.5" />
+            <span>실패 {syncResult.failCount}건</span>
+          </div>
+        </div>
       </ConfirmModal>
     </div>
   );
